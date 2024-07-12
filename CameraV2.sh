3cc57f617dd3855733cd1104cd5dd85538148dfa
gjchester <gavin@gavinchester.com>
2024-07-12 07:56:38 +0100

    Update CameraV2.sh

--- CameraV2.sh
+++ CameraV2.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-DATE=$(date +"%Y-%m-%d_%H%M%S") 
-/usr/bin/raspistill -vf -hf -w 1920 -h 1080 -ISO 800 -ss 6000000 -br 80 -co 100 -t 57600000 -tl 50000 -o /home/pi/camera/$DATE%05d.jpg
+
+#RPICam/LibCamera has more options, this needs a newer version of the OS than V1, but at least it not so obselete.
+#Tested on 32Bit Raspbian Release  5.2 March 2024
+#
+#This script should take an image, and then sleep for 30 second and take another bwefore exiting.   A Cron Job is set to run this script every minute rather than leaving this running this script.
+#
+
+
+DATE=$(date +"%Y-%m-%d_%H%M")
+rpicam-still -o /home/pi/camera/$DATE.jpg
+sleep 30s # Waits 30 seconds.
+DATE=$(date +"%Y-%m-%d_%H%M")
+rpicam-still -o /home/pi/camera/$DATE.jpg
